bc6045285f7a3c78d9b1ca2ec51da501fb22c627
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2025-02-24 21:04:20 -0400

    modify gitignore

---
+++ sc/jet-gen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(dirname "$(readlink -f "$0")")
+
+#!/bin/bash
+
+# Path to the SQLite database file
+db_file="$DIR/temp.db"
+# Path to the SQL schema file
+schema_file="$DIR/../schema/schema.sql"
+
+# Execute the schema from the file
+sqlite3 "$db_file" < "$schema_file"
+
+echo "Database created successfully!"
+
+SOURCE_PATH="$DIR"/../packages/backend/db/gen
+jet -source=sqlite -dsn="file:///$db_file" -path="$SOURCE_PATH"
+rm "$db_file"
