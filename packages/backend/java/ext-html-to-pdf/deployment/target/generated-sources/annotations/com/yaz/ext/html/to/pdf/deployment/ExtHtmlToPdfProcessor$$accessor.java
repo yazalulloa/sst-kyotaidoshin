98b488a9ddcf81f0fb1f040ef5e7519312b1ddb5
Yazal Ulloa <yazalulloa@gmail.com>
2025-09-22 16:01:39 -0400

    remove target

--- packages/backend/java/ext-html-to-pdf/deployment/target/generated-sources/annotations/com/yaz/ext/html/to/pdf/deployment/ExtHtmlToPdfProcessor$$accessor.java
+++
@@ -1,8 +0,0 @@
-package com.yaz.ext.html.to.pdf.deployment;
-@io.quarkus.Generated("Quarkus annotation processor")
-public final class ExtHtmlToPdfProcessor$$accessor {
-    private ExtHtmlToPdfProcessor$$accessor() {}
-    public static Object construct() {
-        return new ExtHtmlToPdfProcessor();
-    }
-}
